cef3a02b4f52fc478a73655d1cecfb871aab8df7
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-03-01 13:15:44 +0100

    Set up page imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  Private,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>;
+  return (
+    <div>
+      <h4>comfy sloth starter</h4>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
