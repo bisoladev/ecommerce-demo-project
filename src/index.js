58db1ce96b83ddf180bfaca03be4fa1f97718f52
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-03-03 01:50:55 +0100

    Imported ProductsProvider into index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProductsProvider>
+      <App />
+    </ProductsProvider>
   </React.StrictMode>
 );
