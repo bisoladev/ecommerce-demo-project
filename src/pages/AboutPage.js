007d8382770eacfa1ee9b210dc7a547da09a717e
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-03-13 07:08:11 +0100

    About page UI setup

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero />
+      <Wrapper className="page secttion section-center">
+        <img src={aboutImg} alt="Nice desk" />
+        <article>
+          <div className="title">
+            <h2>Our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+            beatae quis earum aperiam accusamus cumque qui ipsam est? Iste
+            dolorem odio necessitatibus assumenda commodi placeat soluta numquam
+            perspiciatis culpa architecto.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +52,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
