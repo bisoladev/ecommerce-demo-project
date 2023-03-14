273b1dea5e43406a24c67c1ea4d1a076bf54b101
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-03-14 18:38:55 +0100

    Set up subscribe section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+            molestias cumque corrupti? Numquam harum perferendis voluptatibus!
+            Tenetur aliquid eos impedit.
+          </p>
+          <form
+            className="contact-form"
+            action="https://formspree.io/f/xknazpya"
+            method="POST"
+          >
+            <input
+              type="email"
+              className="form-input"
+              placeholder="Enter email"
+              name="email"
+              required
+            />
+            <button type="submit" className="submit-btn">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +96,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
