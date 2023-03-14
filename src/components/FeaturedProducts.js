5bfe510009424ba577528f3b478ce26e78ecd8d6
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-03-14 23:30:41 +0100

    Destructured values from useProductscontext

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import { useProductsContext } from '../context/products_context'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Error from './Error'
-import Loading from './Loading'
-import Product from './Product'
+import React from "react";
+import { useProductsContext } from "../context/products_context";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Error from "./Error";
+import Loading from "./Loading";
+import Product from "./Product";
 
 const FeaturedProducts = () => {
-  return <h4>featured products</h4>
-}
+  const {
+    products_loading: loading,
+    products_error: error,
+    featured_products: featured,
+  } = useProductsContext();
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+  return <h4>featured products</h4>;
+};
 
 const Wrapper = styled.section`
   background: var(--clr-grey-10);
@@ -31,6 +43,6 @@ const Wrapper = styled.section`
       grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
     }
   }
-`
+`;
 
-export default FeaturedProducts
+export default FeaturedProducts;
