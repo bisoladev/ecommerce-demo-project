9354ee37389d39f504bed264d2928cb5b95f8324
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-03-16 23:09:04 +0100

    Formatted price figure

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,7 +15,7 @@ const Product = ({ image, name, price, id }) => {
       </div>
       <footer>
         <h5>{name}</h5>
-        <p>{price}</p>
+        <p>${price / 100}</p>
       </footer>
     </Wrapper>
   );
