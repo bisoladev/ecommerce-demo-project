1c9524df19e34076097ef05757a57f39fa3e228e
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-03-18 12:59:21 +0100

    set up image gallery

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -62,7 +62,7 @@ const SingleProductPage = () => {
           Back to products
         </Link>
         <div className="product-center">
-          <ProductImages />
+          <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
             <Stars />
