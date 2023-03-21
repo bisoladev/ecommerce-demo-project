e15da8c31ddff994789ba64440bdaeb1cfc9888d
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-03-21 18:00:56 +0100

    created default state for no product match

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,15 @@ import GridView from "./GridView";
 import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
+  const { filtered_products: products, grid_view } = useFilterContext();
+
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: "none" }}>
+        Sorry, no products matched your search...
+      </h5>
+    );
+  }
   return <GridView products={products}>product list</GridView>;
 };
 
