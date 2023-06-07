5fedb91ed2227328943439e760d522c0ca639c85
Shekinah Adenike Adaramola <s.adaramola@rgu.ac.uk>
2023-06-07 06:15:03 +0100

    Created cart buttons

--- src/components/CartContent.js
+++ src/components/CartContent.js
@@ -14,6 +14,17 @@ const CartContent = () => {
       {cart.map((item) => {
         return <CartItem key={item.id} {...item} />;
       })}
+      <hr />
+      <div className="link-container">
+        <Link to="/products" className="link-btn">
+          {" "}
+          Continue shopping
+        </Link>
+        <button className="link-btn clear-btn" onClick={clearCart}>
+          Clear shopping cart
+        </button>
+      </div>
+      <CartTotals />
     </Wrapper>
   );
 };
