c190dffc13a06f227b1d981e7d0f2648dc78660f
Shekinah Adenike Adaramola <s.adaramola@rgu.ac.uk>
2023-06-12 18:10:33 +0100

    Created cart totals

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,13 +8,14 @@ import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
+  const { total_items } = useCartContext();
   return (
     <Wrapper className="cart-btn-wrapper">
       <Link className="cart-btn" to="/cart" onClick={closeSidebar}>
         Cart{" "}
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
       <button className="auth-btn" type="button">
--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -35,15 +35,22 @@ export const CartProvider = ({ children }) => {
   };
 
   //remove item
-  const removeItem = (id) => {};
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: id });
+  };
 
   //toggle amount
-  const toggleAmount = (id, value) => {};
+  const toggleAmount = (id, value) => {
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } });
+  };
 
   //clear cart
-  const clearCart = () => {};
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
 
   useEffect(() => {
+    dispatch({ type: COUNT_CART_TOTALS });
     localStorage.setItem("cart", JSON.stringify(state.cart));
   }, [state.cart]);
 
